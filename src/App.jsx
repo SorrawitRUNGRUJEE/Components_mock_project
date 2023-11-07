01b887d2677e54f178ccf6eb1edaa17fd9e4c070
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2023-11-07 12:41:37 +0700

    logic for conditional

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,24 @@ import {gapi} from "gapi-script"
 import { useEffect } from "react"
 import Components from "./test"
 import Input from "./input"
+import { useState } from "react"
 
 function App() {
 
-  const arr = [
-    {name:"mek",age:26},
-    {name:"tee",age:26},
-    {name:"ice",age:26},
-    {name:"jame",age:26},
-    {name:"rita",age:26},
-    {name:"non",age:26},
 
-  ]
-  const clientId = "25769644481-it9q9s3alpf30c274qlqgepm7tppqr07.apps.googleusercontent.com"
-  useEffect(()=>{
-    const initClient = () =>{
-      gapi.client.init({
-        clientId:clientId,
-        scope:""
-      })
-    }
-    gapi.load("client:auth2",initClient)
-  },[])
-  const onSuccess = (res) => {
-    console.log("success!",res)
-  }
-  const onfail = (res) => {
-    console.log("failed...",res)
-  }
 
-  const click = () =>{
-    console.log("click")
-  }
-  return (
+const [active,setActive] = useState(false)
+
+
+
   
-  <div>
-    {arr.map((el,id)=>{
-       return (
-        <>
-        <Components onClick={click} name={el.name} age={el.age} id={el.id}/>    
-        <Input onClick={2} placeholder={el.name}  />  
-        </>
-          )
-      
-    })}
-  <GoogleLogin 
-    clientId={clientId}
-    buttonText="sign in with google"
-    onSuccess={onSuccess}
-    onFailure={onfail}
-    cookiePolicy={'single_host_origin'}
-    isSignedIn={true}
-    /> 
+  return (
+
+    <div className="">
+      <h1 className={active?"text-red-500":"text-blue-500"}>hello</h1>
+      <button onClick={()=>setActive(!active)} className=" p-4 bg-slate-600">change state</button>
     </div>
+  
     )
 }
 
