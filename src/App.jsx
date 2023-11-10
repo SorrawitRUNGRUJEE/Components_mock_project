cfa06eb64fb0cb8ad37ef454ee1547e8888645f1
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2023-11-10 09:45:20 +0700

    logic for search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import {GoogleLogin} from "react-google-login"
-import {gapi} from "gapi-script"
-import { useEffect } from "react"
-import Components from "./test"
-import Input from "./input"
-import { useState } from "react"
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
+import { useEffect } from "react";
+import Components from "./test";
+import Input from "./input";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const data = [
+    { name: "CQK" },
+    { name: "Otoya" },
+    { name: "Chester grill" },
+    { name: "kagonoya" },
+    { name: "BBQ plaza" },
+  ];
 
+  const hdl_search = (e) => {
+    let result = [];
+    setInput(e.target.value);
+    for (let i of data) {
+      let input = e.target.value.trim().toUpperCase();
+      let shopList = i.name.trim().toUpperCase();
+      if (shopList.includes(input)) {
+        result.push(i);
+      }
+    }
+  };
 
-
-const [active,setActive] = useState(false)
-
-
-
-  
   return (
-
-    <div className="">
-      <h1 className={active?"text-red-500":"text-blue-500"}>hello</h1>
-      <button onClick={()=>setActive(!active)} className=" p-4 bg-slate-600">change state</button>
-<div
-className=" mobile:bg-red-500 desktop:bg-green-500 min-w-full"
-> responsesive</div>
-    </div>
-
-
-
-  
-    )
+    <input
+      className=" border border-solid"
+      value={input}
+      onChange={hdl_search}
+    />
+  );
 }
 
-export default App
+export default App;
